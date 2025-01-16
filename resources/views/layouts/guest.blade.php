bb20e60a91aad4d053a7d67d0252771f0abd8004
Bartosz <MaciejewskiBartosz123@wp.pl>
2025-01-16 20:05:42 +0100

    Logowanie i Rejestracja Logo
    Aktualizacja logo w logowaniu i rejestracji

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -18,7 +18,9 @@
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             <div>
                 <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                
+                <img src="{{ asset('images/large2.png') }}" alt="Logo" class="custom-logo">
+                    
                 </a>
             </div>
 
